c3be56e5830472e33cfed4e43e44e83a8a2ec6fb
Kushal Das <mail@kushaldas.in>
2021-04-08 19:15:11 +0530

    Prints hello world example

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+fn hello() {
+    println!("Hello World!");
 }
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn randomos(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction! (hello, m)?)?;
+
+    Ok(())
+}
+
