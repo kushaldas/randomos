c014d388e395fc5c09bddc25ae0e25960177e08e
Kushal Das <mail@kushaldas.in>
2021-04-16 19:46:48 +0530

    Adds missing docs
    i have a class
    
    Adds allow dead code
    
    Adds add_numbers example
    
    Adds exists method
    
    Adds two more methods
    
    Adds parent function to the Rpath class
    
    Adds one question function
    
    Adds two more methods
    
    Adds parent function to the Rpath class
    
    Adds one question function
    
    Adds two more methods
    
    Adds Ros class
    
    Updates sysinfo crate
    
    Adds add_numbers example
    
    Adds read_file.py example
    
    Adds useless.py example to handle dict
    
    Adds add_numbers example
    
    Adds two more methods
    
    Adds parent function to the Rpath class
    
    Adds one question function
    
    Adds two more methods
    
    Adds Ros class
    
    Updates sysinfo crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
+#![allow(unused_imports)]
 use pyo3::create_exception;
 use pyo3::exceptions::*;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use pyo3::wrap_pyfunction;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
-
+use std::path::Path;
+use sysinfo::{CpuExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 // Exception when you try to add small numbers
 create_exception!(randomos, SmallNumberError, PyException);
+create_exception!(randomos, CPUError, PyException);
 
-/// Says hello to the name given. Returns a string.
+/// Says hello to the name given and welcome to the city. Returns a string.
+/// Takes a dictrionary as argument.
 #[pyfunction]
-#[pyo3(text_signature = "(name)")]
-fn hello(name: String) -> PyResult<String> {
-    let answer = format!("Hello {}", name);
+#[pyo3(text_signature = "(data)")]
+fn hello(data: HashMap<String, String>) -> PyResult<String> {
+    let name = data.get("name").unwrap();
+    let city = data.get("city").unwrap();
+    let answer = format!("Hello {}, welcome to {}", name, city);
     Ok(answer)
 }
 
@@ -41,13 +48,80 @@ fn read_file(py: Python, filename: String) -> PyResult<PyObject> {
     Ok(res.into())
 }
 
+///Class Rpath for path related methods
+#[allow(dead_code)]
+#[pyclass]
+struct Rpath {
+    #[pyo3(get, set)]
+    original_path: String,
+}
+
+#[pymethods]
+impl Rpath {
+    #[new]
+    fn new(original_path: String) -> Self {
+        Rpath { original_path }
+    }
+
+    pub fn exists(&mut self) -> PyResult<bool> {
+        let p = &self.original_path.as_str();
+        let path = Path::new(p);
+        Ok(path.exists())
+    }
+    pub fn is_dir(&mut self) -> PyResult<bool> {
+        let p = &self.original_path.as_str();
+        let path = Path::new(p);
+        Ok(path.is_dir())
+    }
+    pub fn parent(&mut self) -> PyResult<String> {
+        let p = &self.original_path.as_str();
+        let path = Path::new(p);
+        let parent = path.parent().unwrap();
+        let ppath = parent.to_str().unwrap().to_string();
+        return Ok(ppath);
+    }
+
+    pub fn is_file(&mut self) -> PyResult<bool> {
+        let p = &self.original_path.as_str();
+        let path = Path::new(p);
+        Ok(path.is_file())
+    }
+}
+
+///Class Ros for os related methods
+#[pyclass]
+struct Ros {
+    sys: sysinfo::System,
+}
+
+#[pymethods]
+impl Ros {
+    #[new]
+    fn new() -> Self {
+        let sys = System::new_all();
+        Ros { sys }
+    }
+
+    fn number_of_processors(&mut self) -> PyResult<usize> {
+        match self.sys.physical_core_count() {
+            Some(n) => {
+                return Ok(n);
+            }
+            None => return Err(CPUError::new_err("")),
+        }
+    }
+}
+
 /// A Python module implemented in Rust with random OS things.
 #[pymodule]
-fn randomos(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn randomos(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello, m)?)?;
     m.add_function(wrap_pyfunction!(add_numbers, m)?)?;
     m.add_function(wrap_pyfunction!(read_file, m)?)?;
-    m.add("SmallNumberError", _py.get_type::<SmallNumberError>())?;
+    m.add("SmallNumberError", py.get_type_bound::<SmallNumberError>())?;
+    m.add("CPUError", py.get_type_bound::<CPUError>())?;
+    m.add_class::<Rpath>()?;
+    m.add_class::<Ros>()?;
 
     Ok(())
 }
