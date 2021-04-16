6043a58dd6751a9b44fa382d918c482ef62e881b
Kushal Das <mail@kushaldas.in>
2021-04-16 19:46:48 +0530

    Adds missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+/// Says hello to the name given. Returns a string.
 #[pyfunction]
-fn hello(name: String) -> PyResult<String>{
+#[text_signature = "(name)"]
+fn hello(name: String) -> PyResult<String> {
     let answer = format!("Hello {}", name);
     Ok(answer)
 }
@@ -10,8 +12,7 @@ fn hello(name: String) -> PyResult<String>{
 /// A Python module implemented in Rust.
 #[pymodule]
 fn randomos(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction! (hello, m)?)?;
+    m.add_function(wrap_pyfunction!(hello, m)?)?;
 
     Ok(())
 }
-
